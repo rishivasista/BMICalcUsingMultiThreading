09d7f38e772e30b39265f943b6118b92eb0bb3c0
Rishi Vasista <vasistarishi@gmail.com>
2022-10-23 22:46:35 +0530

    Change If to switch in Client.java

--- client/Client.java
+++ client/Client.java
@@ -1,5 +1,6 @@
 package client;
 // Java implementation for a client
+
 // Save file as Client.java
 
 import java.io.*;
@@ -7,83 +8,84 @@ import java.net.*;
 import java.util.Scanner;
 
 // Client class
-public class Client
-{   
-    static final int PORT = 5056;
-	public static void main(String[] args) throws IOException
-	{
-		try
-		{
+public class Client {
+	static final int PORT = 5056;
+
+	public static void main(String[] args) throws IOException {
+		int height;
+		int weight;
+		try {
 			Scanner scn = new Scanner(System.in);
-			
+
 			// getting localhost ip
 			InetAddress ip = InetAddress.getByName("localhost");
-            
+
 			// establish the connection with server port 5056
 			Socket s = new Socket(ip, PORT);
-            System.out.println("Connect on IP "+ip+" and Port " + PORT);
+			System.out.println("Connect on IP " + ip + " and Port " + PORT);
 			// obtaining input and out streams
 			DataInputStream dis = new DataInputStream(s.getInputStream());
 			DataOutputStream dos = new DataOutputStream(s.getOutputStream());
-	
+
 			// the following loop performs the exchange of
 			// information between client and client handler
-			while (true)
-			{
+			while (true) {
 				System.out.println(dis.readUTF());
 				String tosend = scn.nextLine();
 				dos.writeUTF(tosend);
-				
-                if(tosend.toLowerCase().equals("bmi")){
-                    System.out.println(dis.readUTF());
-                    int weight = scn.nextInt();
-                    dos.writeInt(weight);
-                    
-                    System.out.println(dis.readUTF());
-                    int height = scn.nextInt();
-                    dos.writeInt(height);
-                }
 
-                else if(tosend.toLowerCase().equals("bmr")){
-                    System.out.println(dis.readUTF());
-                    int weight = scn.nextInt();
-                    dos.writeInt(weight);
-                    
-                    System.out.println(dis.readUTF());
-                    int height = scn.nextInt();
-                    dos.writeInt(height);
+				switch (tosend.toLowerCase()) {
+					case "bmi":
+						System.out.println(dis.readUTF());
+						weight = scn.nextInt();
+						dos.writeInt(weight);
 
-                    System.out.println(dis.readUTF());
-                    int age = scn.nextInt();
-                    dos.writeInt(age);
-                    
-                    System.out.println(dis.readUTF());
-                    int gender = scn.nextInt();
-                    dos.writeInt(gender);
-                }
-				// If client sends exit,close this connection
-				// and then break from the while loop
-				else if(tosend.toLowerCase().equals("exit"))
-				{
-					System.out.println("Closing this connection : " + s);
-					s.close();
-					System.out.println("Connection closed");
-					break;
+						System.out.println(dis.readUTF());
+						height = scn.nextInt();
+						dos.writeInt(height);
+						break;
+
+					case "bmr":
+						System.out.println(dis.readUTF());
+						weight = scn.nextInt();
+						dos.writeInt(weight);
+
+						System.out.println(dis.readUTF());
+						height = scn.nextInt();
+						dos.writeInt(height);
+
+						System.out.println(dis.readUTF());
+						int age = scn.nextInt();
+						dos.writeInt(age);
+
+						System.out.println(dis.readUTF());
+						int gender = scn.nextInt();
+						dos.writeInt(gender);
+						break;
+
+					case "exit":
+						System.out.println("Closing this connection : " + s);
+						s.close();
+						System.out.println("Connection closed");
+						scn.close();
+						dis.close();
+						dos.close();
+						break;
+
+					default:
+						System.out.println("Invalid Choice!!");
+						break;
+
 				}
-                else {
-                    System.out.println("Invalid Choice");
-                }
-				
-				// printing date or time as requested by client
+
+				// printing bmi result or bmr result as requested by client
 				String received = dis.readUTF();
 				System.out.println(received);
 			}
-			
+
 			// closing resources
-			scn.close();
-			dis.close();
-			dos.close();
-		}catch(Exception e){
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
